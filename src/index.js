51b0ca53d05ead89f47ae2431107bb8afadbd2f8
rio <rhanantyoko@gmail.com>
2023-04-06 00:03:18 +0700

    checkpoint. data dynamically rendered, still tweaking screen responsiveness

---
+++ src/index.js
@@ -0,0 +1,179 @@
+const data = [
+  {
+    id: 1,
+    name: "Noir OLV75",
+    type: "Prebuilt",
+    description:
+      "Featuring south facing hotswap PCB, JWK ultimate black switch with 62g actuation point,  and triple connection mode, the Noir OLV75 is one of the best prebuilts out there",
+    price: 1450000,
+    imageUrl1:
+      "https://cdn.shopify.com/s/files/1/0506/2741/2156/products/OLIVE75-6low_1024x1024@2x.jpg?v=1641444947",
+    imageUrl2: "https://cf.shopee.co.id/file/b49d2451cd09519de5d5c174eeee7460",
+    videolink:
+      "https://www.youtube.com/watch?v=6baEn59H81E&ab_channel=CrankyTechID",
+  },
+  {
+    id: 2,
+    name: "Rexus Daxa M64 Classic",
+    type: "Prebuilt",
+    description:
+      "Featuring south facing hotswap PCB, gateron yellow switch with 55g actuation, and usb type c detachable, the Rexus Daxa M64 classic is a budget killer prebuilt. Sold in grey color",
+    price: 750000,
+    imageUrl1:
+      "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/2/10/8d343d19-ccad-4442-a6cb-c371065030e0.jpg",
+    imageUrl2: "https://cf.shopee.co.id/file/4df322c306f49ec6d9fbd9a5908dfe94",
+    videolink:
+      "https://www.youtube.com/watch?v=SWsx_mx8oWE&ab_channel=CrankyTechID",
+  },
+  {
+    id: 3,
+    name: "Fantech Maxfit67",
+    type: "Prebuilt",
+    description:
+      "Featuring south facing hotswap PCB, gateron yellow switch with 55g actuation, usb type c detachable, and really good stabilizer tuning out of the box, the maxfit is something that you dont have to tinker with out of the box and it will still compete with the big boys out there",
+    price: 1250000,
+    imageUrl1:
+      "https://www.techpowerup.com/review/fantech-maxfit67-rgb/images/keyboard-1.jpg",
+    imageUrl2:
+      "https://asset.kompas.com/crops/fE3JUMlh_C7tTSnjy_NxWR0Bilo=/1x0:2000x1333/780x390/data/photo/2022/01/20/61e855b985854.jpg",
+    videolink:
+      "https://www.youtube.com/watch?v=YdNeHq7Xruo&ab_channel=CrankyTechID",
+  },
+  {
+    id: 4,
+    name: "Keychron V1",
+    type: "Prebuilt",
+    description:
+      "This budget version of the Q1, but wow does it not feel like that. Featuring keychron own red switch with 45g of actuation, doubel shot OSA profile keycaps, and southfacing pcb, surely the keychron V1 is not something to be underestimate.",
+    price: 1700000,
+    imageUrl1:
+      "https://cdn.shopify.com/s/files/1/0059/0630/1017/t/5/assets/keychronv1custommechanicalkeyboard2-1657168313809.jpg?v=1657168321",
+    imageUrl2:
+      "https://cdn.shopify.com/s/files/1/0059/0630/1017/t/5/assets/keychronv1custommechanicalkeyboard4-1657168342318.jpg?v=1657168355",
+    videolink:
+      "https://www.youtube.com/watch?v=HVphB6RYBaY&ab_channel=HardwareCanucks",
+  },
+  {
+    id: 5,
+    name: "Keyhron Q1",
+    type: "Prebuilt",
+    description:
+      "Gasket, gasket, gasket!. Being the only gasket option on our prebuilt lineup and equipped with gateron yellow switch, the keychron Q1 is one of a teeny tiny population of solid, enthusiast level prebuilt out there",
+    price: 2800000,
+    imageUrl1:
+      "https://ae01.alicdn.com/kf/H19b23c33870643699f10b4682c74e006o/Keychron-Q1-QMK-Keyboard-Mekanis-Kustom-75-Barebone-ISO-Layout-Type-c-Keyboard-Berkabel-untuk-Mac.jpg",
+    imageUrl2:
+      "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/10/9/caa43e5f-a8b0-4d94-8451-7859d34b1890.jpg",
+    videolink:
+      "https://www.youtube.com/watch?v=d9v5bK8hCMM&ab_channel=CrankyTechID",
+  },
+  {
+    id: 6,
+    name: "KBD67 Lite R4",
+    type: "barebones",
+    description:
+      "The KBD67 lite R4 is a 'budget' option from kbdfans that features a southfacing pcb, a translucent case, and a gasket mounted system. Seriously, nothing could go wrong with this kit ",
+    price: 1600000,
+    imageUrl1:
+      "https://cdn.shopify.com/s/files/1/1473/3902/products/c184defbe2b9cac90d93ed6156093c5f_460x.jpg?v=1653029338f",
+    imageUrl2:
+      "https://cdn.shopify.com/s/files/1/1473/3902/products/18_61a9e438-9c13-4dd9-b125-53c2fa99f530_460x.jpg?v=1653029338",
+    videolink:
+      "https://www.youtube.com/watch?v=TE_uXoOeIuU&t=50s&ab_channel=HamajiNeo",
+  },
+  {
+    id: 7,
+    name: "Meletrix Zoom65 Essential Edition",
+    type: "barebones",
+    description:
+      "The zoom65 is taking the market by the storm! hurry and grab it fast while it last! no keycaps and switch included, it is a barebone after all",
+    price: 2400000,
+    imageUrl1:
+      "https://cdn.shopify.com/s/files/1/0238/7342/1376/products/ZOOM65-Black.jpg?v=1649232801",
+    imageUrl2:
+      "https://cdn.shopify.com/s/files/1/0607/2474/0287/products/ZOOM65_19db3da9-483f-4cd8-ba1b-81d40c1481c7_600x600.jpg?v=1649002460",
+    videolink:
+      "https://www.youtube.com/watch?v=6dB6CjzHhLQ&ab_channel=%3A3ildcat",
+  },
+  {
+    id: 8,
+    name: "KBDfans Tiger 80",
+    type: "barebones",
+    description:
+      "Our only TKL pick for TKL lovers out there. The Tiger 80 is packed with robust construction, gasket mounted, and a case color that is worth the money.",
+    price: 3400000,
+    imageUrl1:
+      "https://cdn.shopify.com/s/files/1/1473/3902/products/8_6a27b87b-88b8-4fb3-a85f-37de03a184f5_460x.jpg?v=1653037121",
+    imageUrl2:
+      "https://cdn.shopify.com/s/files/1/1473/3902/products/6_a09a59c8-6281-41c2-9216-909c75e838ac_460x.jpg?v=1653037121",
+    videolink:
+      "https://www.youtube.com/watch?v=LyH4vO4wx6I&t=843s&ab_channel=Keybored",
+  },
+  {
+    id: 9,
+    name: "KBDFans Tofu60",
+    type: "barebones",
+    description:
+      "This thing is timeless. With a solid aluminium case, brass plate, and a solid look, nothing can ever go wrong with this barebone kit",
+    price: 2300000,
+    imageUrl1:
+      "https://cdn.shopify.com/s/files/1/1473/3902/products/63_40f8487a-49cc-4686-bdbf-619512f83805_460x.jpg?v=1639025220",
+    imageUrl2:
+      "https://cdn.shopify.com/s/files/1/1473/3902/products/12_5c20edc0-ec8a-47ed-b649-fee0c2539967_460x.jpg?v=1619687748",
+    videolink:
+      "https://www.youtube.com/watch?v=hvyTHpitIQ8&t=364s&ab_channel=OptimumTech",
+  },
+  {
+    id: 10,
+    name: "Akko mod 005",
+    type: "barebones",
+    description:
+      "The solid 65% options from us. With gasket mounted and solid aluminium case, surely the MOD 005 would satisfy your needs",
+    price: 1900000,
+    imageUrl1:
+      "https://en.akkogear.com/wp-content/uploads/2021/11/MOD005-Dark-Night.jpg",
+    imageUrl2:
+      "https://en.akkogear.com/wp-content/uploads/2021/11/MOD005-SP4-1024x1024.jpg",
+    videolink:
+      "https://www.youtube.com/watch?v=tREBIaoa0N4&t=416s&ab_channel=CGBuild",
+  },
+  {
+    id: 11,
+    name: "Noir OLV75 Barebone",
+    type: "barebones",
+    description:
+      "Local pride! This is the barebones version from the Noir OLV75 fullkit, packing the same features just without the keycaps and switches. Because those two are for you to explore!",
+    price: 900000,
+    imageUrl1:
+      "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/4/19/39280819-070d-4d05-9015-5735e3b05380.jpg",
+    imageUrl2:
+      "https://cdn.shopify.com/s/files/1/0506/2741/2156/products/OLV75Barebone_2of1_-Copy_1024x1024@2x.jpg?v=1649734579",
+    videolink:
+      "https://www.youtube.com/watch?v=6baEn59H81E&ab_channel=CrankyTechID",
+  },
+];
+
+let htmlCode = "";
+
+for (let i = 0; i < data.length; i++) {
+  htmlCode =
+    htmlCode +
+    `
+    <div class="column">
+    <div class="card">
+    <img src="${data[i].imageUrl1}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h4 class="card-title">${data[i].name}</h4>
+    <p class="card-text">${data[i].description}</p>
+    <p class="card-text"><b>Type</b>: ${data[i].type}</p>
+    <p class="card-text"><b>Price</b>: Rp. ${data[i].price}</p>
+    <button type="button" class="btn btn-light"><a href="${data[i].videolink}" style="text-decoration:none; color:black" target="_blank">Full Product Review</a></button>
+    </div>
+    </div>
+    </div>
+    `;
+}
+
+const productCards = document.querySelector(".allProductCards");
+
+productCards.innerHTML = htmlCode;
